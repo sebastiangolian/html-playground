3e55f47be3935222a2b48e1e6068a14ed4568a4e
Sebastian Golian <sebastiangolian@gmail.com>
2020-10-20 20:21:45 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
@@ -15,6 +16,14 @@ module.exports = {
     watchContentBase: true
   },
   entry: './src/script.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebPackPlugin({
         template: "./src/index.html",
@@ -22,5 +31,9 @@ module.exports = {
         favicon: "./src/favicon.ico",
         minify: true
     }),
+    new MiniCssExtractPlugin({
+      filename: "./css/[name].[hash].css",
+      chunkFilename: "[id].css"
+  })
 ]
 };
